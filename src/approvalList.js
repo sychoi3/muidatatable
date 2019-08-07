afe597c86badc3fb786bb93c059e85e17ea32131
simon <siimon.choi@gmail.com>
2019-08-07 15:21:55 -0700

    refactor
    refactor data.

---
+++ src/approvalList.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const approvals = [
+  {
+    approvalId: 8,
+    programCode: "prime_lte",
+    tierNumber: 1,
+    locationId: "0207",
+    locationName: "Club USA",
+    createdDate: "2019-08-07T18:12:58.167",
+    createdBy: "MigrateFitNetData",
+    lastModifiedDate: "2019-08-07T18:12:58.167",
+    lastModifiedBy: "MigrateFitNetData",
+    startDate: "2019-06-01T00:00:00"
+  },
+  {
+    approvalId: 13,
+    programCode: "prime_lte",
+    tierNumber: 1,
+    locationId: "0243",
+    locationName: "EOS Fitness - Las Vegas South",
+    createdDate: "2019-08-07T18:12:58.167",
+    createdBy: "MigrateFitNetData",
+    lastModifiedDate: "2019-08-07T18:12:58.167",
+    lastModifiedBy: "MigrateFitNetData",
+    startDate: "2015-08-01T00:00:00"
+  },
+  {
+    approvalId: 15,
+    programCode: "prime_lte",
+    tierNumber: 1,
+    locationId: "0283",
+    locationName: "EOS Fitness - Palm Desert",
+    createdDate: "2019-08-07T18:12:58.167",
+    createdBy: "MigrateFitNetData",
+    lastModifiedDate: "2019-08-07T18:12:58.167",
+    lastModifiedBy: "MigrateFitNetData",
+    startDate: "2015-08-01T00:00:00"
+  }
+];
